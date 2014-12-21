8456faa6095a24a275b4e8d670cc6f137ae10a19
Namone <al2xnord@yahoo.com>
2014-12-20 21:32:13 -0700

    Collision tweaking - still needs work - much better

--- src/com/namone/collision/WorldCollision.java
+++ src/com/namone/collision/WorldCollision.java
@@ -3,6 +3,8 @@ package com.namone.collision;
 import java.util.ArrayList;
  
 
+
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.geom.Rectangle;
  
 import org.newdawn.slick.geom.Vector2f;
@@ -40,9 +42,11 @@ public class WorldCollision {
                                 
                                 // IF THE RIGHT SIDE OF PLAYER HITBOX IS EQUAL TO
                                 // LEFT SIDE OF BLOCK HITBOX
-                                if(hitboxLocation.getX() + 32 == blockLocation.getX()) {
+                                if(Keyboard.isKeyDown(Keyboard.KEY_D)  
+                                		&& hitboxLocation.getX() + 32 >= blockLocation.getX()
+                                		&& hitboxLocation.getX() + 32 <= blockLocation.getX() + 32) {
                                 	// BUMP THE PLAYER OUT OF COLLISION
-                                	boxX = (int) hitboxLocation.getX() - 1;
+                                	boxX = (int) hitboxLocation.getX() - 3;
                                 	// TESTING
                                 	System.out.println("HIT");
                                 	// SET THE PLAYER'S POSITION EQUAL TO THE 
@@ -52,14 +56,35 @@ public class WorldCollision {
                                 
                                 // IF LEFT SIDE OF PLAYER HITBOX IS EQUAL TO 
                                 // RIGHT SIDE OF BLOCK HTIBOX
-                                else if (hitboxLocation.getX() == blockLocation.getX() + 32) {
+                                else if (Keyboard.isKeyDown(Keyboard.KEY_A)
+                                		&& hitboxLocation.getX() <= blockLocation.getX() + 32
+                                		&& hitboxLocation.getX() >= blockLocation.getX()) {
                                 	// SAME AS ABOVE
-                                	boxX = (int) hitboxLocation.getX() + 1;
+                                	boxX = (int) hitboxLocation.getX() + 3;
                                 	System.out.println("HIT 2");
                                 	player.PlayerX = boxX;    
                                 }
                                 
-                            	player.PlayerX = boxX; 
+                                else if (Keyboard.isKeyDown(Keyboard.KEY_S)
+                                		&& hitboxLocation.getY() + 32 >= blockLocation.getY()
+                                		&& hitboxLocation.getY() + 32 <= blockLocation.getY() + 32) {
+                                	// SAME AS ABOVE
+                                	boxY = (int) hitboxLocation.getY() - 3;
+                                	System.out.println("HIT 2");
+                                	player.PlayerY = boxY;    
+                                }
+                                
+                                else if (Keyboard.isKeyDown(Keyboard.KEY_W) 
+                                		&& hitboxLocation.getY() <= blockLocation.getY() + 32
+                                		&& hitboxLocation.getY() >= blockLocation.getY()) {
+                                	// SAME AS ABOVE
+                                	boxY = (int) hitboxLocation.getY() + 3;
+                                	System.out.println("HIT 2");
+                                	player.PlayerY = boxY;    
+                                }
+
+
+                            
                                                           
                         }
                         
